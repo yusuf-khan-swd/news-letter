1a15630ecc2f73ae83877e5c9837a3f76e284c14
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-09-03 01:11:24 +0600

    load img and title from api

--- js/app.js
+++ js/app.js
@@ -75,17 +75,19 @@ const displayClickedCategoryNews = categoryItems => {
   newsContainer.textContent = '';
 
   categoryItems.forEach(categoryItem => {
+    console.log(categoryItem);
+
     const newsDiv = document.createElement('div');
     newsDiv.classList.add('col');
     newsDiv.innerHTML = `
-    <div class="card mb-3 shadow">
+    <div class="card mb-3 shadow p-3">
     <div class="row g-0">
-      <div class="col-md-4">
-        <img src="..." class="img-fluid rounded-start" alt="..." />
+      <div class="col-md-3">
+        <img src="${categoryItem.thumbnail_url}" class="img-fluid rounded-start" alt="..." />
       </div>
-      <div class="col-md-8">
+      <div class="col-md-9">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <h5 class="card-title">${categoryItem.title}</h5>
           <p class="card-text">
             This is a wider card with supporting text below as a
             natural lead-in to additional content. This content is a
