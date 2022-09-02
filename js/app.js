25e1804e049ef24dc0f721da18946aca6d9d9bc2
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-09-03 01:06:22 +0600

    add ui for news card and load total news for needed category

--- js/app.js
+++ js/app.js
@@ -66,15 +66,43 @@ const loadClickedCategoryNews = async categoryId => {
   catch (err) {
     console.log(err);
   }
-
 };
 
 const displayClickedCategoryNews = categoryItems => {
   console.log(categoryItems.length !== 0 ? categoryItems.length : 'not found')
 
+  const newsContainer = document.getElementById('news-container');
+  newsContainer.textContent = '';
+
   categoryItems.forEach(categoryItem => {
-    console.log(categoryItem);
+    const newsDiv = document.createElement('div');
+    newsDiv.classList.add('col');
+    newsDiv.innerHTML = `
+    <div class="card mb-3 shadow">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="..." class="img-fluid rounded-start" alt="..." />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a
+            natural lead-in to additional content. This content is a
+            little bit longer.
+          </p>
+          <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+    newsContainer.appendChild(newsDiv);
   });
+
+  toggler(false);
 }
 
 loadCategories();
