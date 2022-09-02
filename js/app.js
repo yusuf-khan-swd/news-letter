9f34864fb793a53811382909849ac0d0bb32b75d
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-09-02 22:37:21 +0600

    fetch category and set to html

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
 
-console.log('Hello from app')
+const loadCategories = async () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayCategories(data.data.news_category);
+};
+
+const displayCategories = categories => {
+  const categoryContainer = document.getElementById('categoryContainer');
+
+  categories.forEach(category => {
+    const categoryList = document.createElement('li');
+    categoryList.classList.add('nav-item');
+    categoryList.innerHTML = `
+    <a class="nav-link" href="#">${category.category_name}</a>
+    `;
+
+    categoryContainer.appendChild(categoryList);
+  });
+};
+
+loadCategories();
