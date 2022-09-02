dcef1c812f2381b89423fc3839cd1708f7b7ed76
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-09-03 00:07:26 +0600

    active category dynamically color and active

--- js/app.js
+++ js/app.js
@@ -18,14 +18,26 @@ const displayCategories = categories => {
     const categoryList = document.createElement('li');
     categoryList.classList.add('nav-item');
     categoryList.innerHTML = `
-    <a onclick="loadClickedCategoryNews('${category.category_id}')" class="nav-link" href="#">${category.category_name}</a>
+    <a onclick="loadClickedCategoryNews('${category.category_id}')" class="nav-link ${category.category_id}" href="#">${category.category_name}</a>
     `;
 
     categoryContainer.appendChild(categoryList);
   });
 };
 
+
+
 const loadClickedCategoryNews = async categoryId => {
+  const classList = document.getElementsByClassName('active')
+  for (let i = 0; i < classList.length + 1; i++) {
+    if (classList[i]) {
+      classList[i].classList.remove('text-primary');
+      classList[i].classList.remove('active');
+    }
+  }
+  const clickCategory = document.getElementsByClassName(`${categoryId}`)[0];
+  clickCategory.classList.add('text-primary');
+  clickCategory.classList.add('active');
   try {
     const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
     const res = await fetch(url);
@@ -39,6 +51,7 @@ const loadClickedCategoryNews = async categoryId => {
 };
 
 const displayClickedCategoryNews = categoryItems => {
+  console.log(categoryItems.length !== 0 ? categoryItems : 'not found')
 
   categoryItems.forEach(categoryItem => {
     console.log(categoryItem);
